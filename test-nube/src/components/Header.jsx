801ab1d3280d3495356085c7ca383ef9449d0758
Guillermo Martínez <guillermogabrielmartinezbarros@gmail.com>
2024-11-18 22:23:30 -0300

    Update Header.jsx

--- test-nube/src/components/Header.jsx
+++ test-nube/src/components/Header.jsx
@@ -21,13 +21,13 @@ export default function Header() {
                 </span>
 
                 {/* To do: Simplificar */}
-                {!loadingSession && session && path === "/admin" && <Link href={"/"} className="flex items-center gap-2">Go to Home
+                {!loadingSession && session && path === "/admin" && <Link href={"/"} className="flex items-center gap-2">Home
                     <svg width={"16px"} height={"16px"} viewBox="0 0 24 24" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
                         <g xmlns="http://www.w3.org/2000/svg" id="SVGRepo_iconCarrier"> <path d="M16.3153 16.6681C15.9247 17.0587 15.9247 17.6918 16.3153 18.0824C16.7058 18.4729 17.339 18.4729 17.7295 18.0824L22.3951 13.4168C23.1761 12.6357 23.1761 11.3694 22.3951 10.5883L17.7266 5.9199C17.3361 5.52938 16.703 5.52938 16.3124 5.91991C15.9219 6.31043 15.9219 6.9436 16.3124 7.33412L19.9785 11.0002L2 11.0002C1.44772 11.0002 1 11.4479 1 12.0002C1 12.5524 1.44772 13.0002 2 13.0002L19.9832 13.0002L16.3153 16.6681Z" fill="#fff" /> </g>
                     </svg>
                 </Link>}
 
-                {!loadingSession && session && path === "/" && <Link href={"/admin"} className="flex items-center gap-2">Go to Admin
+                {!loadingSession && session && path === "/" && <Link href={"/admin"} className="flex items-center gap-2">Admin
                     <svg width={"16px"} height={"16px"} viewBox="0 0 24 24" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
                         <g xmlns="http://www.w3.org/2000/svg" id="SVGRepo_iconCarrier"> <path d="M16.3153 16.6681C15.9247 17.0587 15.9247 17.6918 16.3153 18.0824C16.7058 18.4729 17.339 18.4729 17.7295 18.0824L22.3951 13.4168C23.1761 12.6357 23.1761 11.3694 22.3951 10.5883L17.7266 5.9199C17.3361 5.52938 16.703 5.52938 16.3124 5.91991C15.9219 6.31043 15.9219 6.9436 16.3124 7.33412L19.9785 11.0002L2 11.0002C1.44772 11.0002 1 11.4479 1 12.0002C1 12.5524 1.44772 13.0002 2 13.0002L19.9832 13.0002L16.3153 16.6681Z" fill="#fff" /> </g>
                     </svg></Link>}
@@ -39,7 +39,7 @@ export default function Header() {
                 </Link>}
 
                 {path === "/login" && <Link href={"/"} className="flex items-center gap-2">
-                    Go to Home
+                    Home
                     <svg width={"16px"} height={"16px"} viewBox="0 0 24 24" fill="#ffffff" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
                         <g xmlns="http://www.w3.org/2000/svg" id="SVGRepo_iconCarrier"> <path d="M16.3153 16.6681C15.9247 17.0587 15.9247 17.6918 16.3153 18.0824C16.7058 18.4729 17.339 18.4729 17.7295 18.0824L22.3951 13.4168C23.1761 12.6357 23.1761 11.3694 22.3951 10.5883L17.7266 5.9199C17.3361 5.52938 16.703 5.52938 16.3124 5.91991C15.9219 6.31043 15.9219 6.9436 16.3124 7.33412L19.9785 11.0002L2 11.0002C1.44772 11.0002 1 11.4479 1 12.0002C1 12.5524 1.44772 13.0002 2 13.0002L19.9832 13.0002L16.3153 16.6681Z" fill="#fff" /> </g>
                     </svg></Link>}
